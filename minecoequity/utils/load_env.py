939dbb4aa960155dbcb58afd45c89fd1d98fd956
Jeremiah David <jeremiahedavid@gmail.com>
2024-04-22 21:59:48 +0100

    resolved a new redis_url

--- minecoequity/utils/load_env.py
+++ minecoequity/utils/load_env.py
@@ -7,7 +7,7 @@ from minecoequity.utils.logger import LOGGER
 env = config
 
 TOKEN: Final = env('TOKEN')
-REDIS_URL: Final = env('REDIS_URL')
+REDIS_URL: Final = env('REDIS_URL', default="redis://:6a30851e71dc192e854a841bab63053a4297fb31c6569a1f11c76d0f7fd0c865@dokku-redis-mincorequit-rd:6379")
 USERNAME: Final = env('USERNAME')
 DEVELOPER_CHAT_ID: Final = env('DEVELOPER_CHAT_ID')
 COINBASE_API: Final = env('COINBASE_API')
